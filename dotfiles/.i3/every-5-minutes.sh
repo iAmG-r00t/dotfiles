3c7b3dc7b2f5f35457f87ae08857d4f8798f3b89
iAmG-r00t <numimickey2@gmail.com>
2021-11-02 04:38:37 +0300

    Add new dotfiles from the other repo to this repo... 😂 👨‍ 💻 😎 ✍  👾

--- dotfiles/.i3/every-5-minutes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true
-do
- ~/.dotfiles/.i3/suspendbattery.sh
- ~/.dotfiles/.i3/lowbatt.sh
-sleep 15
-done
--- dotfiles/.omz/custom/vim-plugins.zsh
+++
@@ -1,5 +0,0 @@
-alias vim-plugins=' echo -e "
-\033[1;33mDo this after new desktop setup \033[0;31m\e[5m(Configurations)\033[0m
-\e[96mConfiguring YouCompleteMe plugin after installing with Vim-plugin Manager 
-\e[95mEnter Installation directory: \033[0mcd ~/.dotfiles/.vim/plugged/YouCompleteMe
-\e[95mRun this to configure the installation: python3 install.py --clang-completer"'
