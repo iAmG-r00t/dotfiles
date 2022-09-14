16829c2b7766fb52b67fd062357691d8dc3f2f9f
G-r00t <numimickey2@gmail.com>
2022-09-14 06:48:56 -0700

    fix minor syntax

--- server/servers.sh
+++ server/servers.sh
@@ -23,7 +23,7 @@ red="\033[1;31m"
 
 
 # Report errors when file exits out
-[ ! -z "$BASH_SOURCE[@]"] && filename=$(basename "${BASH_SOURCE[@]}")
+[ ! -z "$BASH_SOURCE[@]" ] && filename=$(basename "${BASH_SOURCE[@]}")
 err_report() {
   echo " "
   echo -e "* ${brown}Error on line ${red}$1${reset} file: ${magenta}${filename}${reset}"
