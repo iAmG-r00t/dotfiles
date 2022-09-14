f3463df854c3188f9c4bfd2a6840bbce751a395c
G-r00t <numimickey2@gmail.com>
2022-09-14 07:23:21 -0700

    online debug

--- server/servers.sh
+++ server/servers.sh
@@ -23,10 +23,11 @@ red="\033[1;31m"
 
 
 # Report errors when file exits out
-[ ! -z "$BASH_SOURCE[@]" ] && filename=$(basename "${BASH_SOURCE[@]}")
+#[ ! -z "$BASH_SOURCE[@]" ] && filename=$(basename "${BASH_SOURCE[@]}")
+[ ! -z "$BASH_SOURCE[@]" ] && echo "I am Empty"
 err_report() {
   echo " "
-  echo -e "* ${brown}Error on line ${red}$1${reset} file: ${magenta}${filename}${reset}"
+  #echo -e "* ${brown}Error on line ${red}$1${reset} file: ${magenta}${filename}${reset}"
   echo " "
 }
 
